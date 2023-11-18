4f98a8756f1216b20b0295ff32cbfc23ebdcc7f4
Ryoga-exe <git@ryoga.dev>
2023-11-18 12:03:07 +0900

    fix: remove unused +1

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -366,7 +366,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT posts.id, posts.user_id, posts.body, posts.mime, posts.created_at FROM `posts` JOIN `users` ON users.id = posts.user_id WHERE users.del_flg = 0 ORDER BY posts.created_at DESC LIMIT ?", postsPerPage+1)
+	err := db.Select(&results, "SELECT posts.id, posts.user_id, posts.body, posts.mime, posts.created_at FROM `posts` JOIN `users` ON users.id = posts.user_id WHERE users.del_flg = 0 ORDER BY posts.created_at DESC LIMIT ?", postsPerPage)
 	if err != nil {
 		log.Print(err)
 		return
@@ -500,7 +500,7 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	results := []Post{}
-	err = db.Select(&results, "SELECT posts.id, posts.user_id, posts.body, posts.mime, posts.created_at FROM `posts` JOIN `users` ON posts.user_id = users.id WHERE users.del_flg = 0 AND posts.created_at <= ? ORDER BY `created_at` DESC, LIMIT ?", t.Format(ISO8601Format), postsPerPage+1)
+	err = db.Select(&results, "SELECT posts.id, posts.user_id, posts.body, posts.mime, posts.created_at FROM `posts` JOIN `users` ON posts.user_id = users.id WHERE users.del_flg = 0 AND posts.created_at <= ? ORDER BY `created_at` DESC, LIMIT ?", t.Format(ISO8601Format), postsPerPage)
 
 	if err != nil {
 		log.Print(err)
